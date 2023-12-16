3ba83f4f69e2b3827ffd2c10363edb838c1adc15
eebbesen <eebbesen.git@gmail.com>
2023-12-16 15:55:38 -0600

    rubocop remediation

--- spec/views/messages/show.html.erb_spec.rb
+++ spec/views/messages/show.html.erb_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe 'messages/show' do
   it 'renders attributes' do
     render
 
-    %w(Content Status Sent\ At).each { |h| expect(rendered).to include(h) }
+    ['Content', 'Status', 'Sent At'].each { |h| expect(rendered).to include(h) }
     [message.content, message.status].each { |a| expect(rendered).to include(a) }
   end
 end
