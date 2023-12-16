de78bd38afb58f9aae7bf052601a65d277908341
eebbesen <eebbesen.git@gmail.com>
2023-12-16 12:01:56 -0600

    savepoint

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -9,7 +8,3 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-user = User.first
-
-Message.create!(content: "You've subscribed to park info", user:, status: 'SENT', sent_at: DateTime.now - 5)
-Message.create!(content: 'Upcoming construction', user:, status: 'PENDING')
