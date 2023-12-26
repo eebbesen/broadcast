b6d39af21d73310074fcaa6fb6c6c43da070e47e
eebbesen <eebbesen.git@gmail.com>
2023-12-26 12:40:56 -0600

    logging controller actions

---
+++ app/controllers/concerns/crud_logger.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+# Concern to standardize controller loggging
+module CrudLogger
+  extend ActiveSupport::Concern
+
+  TEMPLATE = '%<class_name>s TYPE in %<method>s with %<data>s'
+  REQUEST = TEMPLATE.gsub('TYPE', 'request')
+  SUCCESS = TEMPLATE.gsub('TYPE', 'success')
+  FAILURE = TEMPLATE.gsub('TYPE', 'failure')
+
+
+  included do
+    # log request to do something (e.g., create/destroy/edit a resource)
+    def log_request(method, data)
+      Rails.logger.debug(format(REQUEST, class_name: self.class.name, method:, data:))
+    end
+
+    def log_success(method, data)
+      Rails.logger.info(format(SUCCESS, class_name: self.class.name, method:, data:))
+    end
+
+    def log_failure(method, data)
+      Rails.logger.error(format(FAILURE, class_name: self.class.name, method:, data:))
+    end
+  end
+
+  # class_methods do
+
+  # end
+end
